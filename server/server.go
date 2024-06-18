7f463844eed277afbd5811f3bf65cd81d70a6f56
Christopher V <ChrisRVill@gmail.com>
2024-06-17 20:08:26 -0700

    First commit for my website

---
+++ server/server.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"html/template"
+	"io"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	e := echo.New()
+	e.Renderer = newTemplate()
+	e.GET("/", StartPage)
+
+	e.Logger.Fatal(e.Start(":8080"))
+}
+
+func StartPage(c echo.Context) error {
+	err := c.Render(http.StatusOK, "index", nil)
+	if err != nil {
+		panic("Couldnt render base index page")
+	}
+	return err
+}
+
+type Template struct {
+	templates *template.Template
+}
+
+func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+func newTemplate() *Template {
+	return &Template{
+		templates: template.Must(template.ParseGlob("public/views/*.html")),
+	}
+}
