56d9d7e59b2fa3370d14330d5e5bc907d35fc924
Christopher V <ChrisRVill@gmail.com>
2024-06-23 21:32:18 -0700

    Layout is finished server sends list

--- server/server.go
+++ server/server.go
@@ -3,37 +3,19 @@ package server
 import (
 	"html/template"
 	"io"
-	"math/rand"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
 )
 
-func Start() {
-	e := echo.New()
-	e.Static("/static", "public")
-	e.Renderer = newTemplate()
-
-	e.GET("/", startPage)
-
-	e.Logger.Fatal(e.Start(":8080"))
+type Template struct {
+	templates *template.Template
 }
 
-// Creates the page based on the templates, but does not pass in any extra info
-func startPage(c echo.Context) error {
-	newPage := Page{Slides: false, Project: newProject()}
-	newPage.funTitles()
-	err := c.Render(http.StatusOK, "index", newPage)
-	if err != nil {
-		panic("Couldnt render base index page")
-	}
-	return err
-}
-
+// All the items the page will need from the server
 type Page struct {
-	Slides         bool
-	PortfolioTitle string
-	Project        *Project
+	Project Project
+	List    []Project
 }
 
 type Project struct {
@@ -42,8 +24,24 @@ type Project struct {
 	Description string
 }
 
-type Template struct {
-	templates *template.Template
+func StartServer() {
+	e := echo.New()
+	e.Static("/static", "public")
+	e.Renderer = newTemplate()
+
+	e.GET("/", getPortfolio)
+
+	e.Logger.Fatal(e.Start(":8080"))
+}
+
+// Creates page and sends important info created as page struct
+func getPortfolio(c echo.Context) error {
+	newPage := newPage("current")
+	err := c.Render(http.StatusOK, "index", newPage)
+	if err != nil {
+		panic("Couldnt render base index page")
+	}
+	return err
 }
 
 func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
@@ -55,20 +53,27 @@ func newTemplate() *Template {
 	}
 }
 
-// Addes a PortfolioTitle to the page struct based on random chance. Default title is My portfolio, but any PortfolioTitle property passed into the templates will update the title
-func (p *Page) funTitles() {
-	if rand.Intn(1001)%500 == 0 {
-		p.PortfolioTitle = "Help!"
-
+func newPage(current string) Page {
+	return Page{
+		Project: newProject(current),
+		List:    newList([]string{current, "Project2", "Project3", "Project4"}),
 	}
 }
 
 // Img will be in /static/images/logos/
 // Img property of Project struct is the name of the svg file
-func newProject() *Project {
-	return &Project{
+func newProject(name string) Project {
+	return Project{
 		Img:         "test",
-		Title:       "Test Project",
-		Description: "A project I am working on",
+		Title:       name,
+		Description: "The Description of the project.",
 	}
+}
+
+func newList(names []string) []Project {
+	list := []Project{}
+	for _, name := range names {
+		list = append(list, newProject(name))
+	}
+	return list
 }
