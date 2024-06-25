0daad5bf2f17ed781cb0f4c825be80e54698131b
Christopher V <ChrisRVill@gmail.com>
2024-06-25 00:23:30 -0700

    Updating CSS to use Viewport as size ref instead of px or flex box scaling. Scaling the window to different sizes has better results now

--- server/projects.go
+++ server/projects.go
@@ -1,21 +1,22 @@
 package server
 
 func createProjectList() []Project {
+	img := []string{"ProjectPort", "test", "test", "test", "test"}
 	names := []string{"Password Generator", "project 2", "project 3", "project 4", "project 5"}
 	description := []string{"Creates a random password using a random seed and phrase entered by the user.", "Help", "me", "!", "5"}
 	link := []string{"https://github.com/CVilledieu/PasswordMaker", "#", "#", "#", "#"}
 	list := []Project{}
 	for i, name := range names {
-		list = append(list, newProject(name, description[i], link[i]))
+		list = append(list, newProject(name, description[i], link[i], img[i]))
 	}
 	return list
 }
 
 // Img will be in /static/images/logos/
 // Img property of Project struct is the name of the svg file
-func newProject(name, description, link string) Project {
+func newProject(name, description, link, img string) Project {
 	return Project{
-		Img:         "test",
+		Img:         img,
 		Title:       name,
 		Description: description,
 		Link:        link,
