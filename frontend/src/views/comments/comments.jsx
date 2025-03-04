ad89f93b7519e40ac30cab6b77b1ee7b0920ec5a
Christopher V <ChrisRVill@gmail.com>
2025-03-04 00:36:20 -0800

    Comments page now uses stated passed as props instead of contect. Comment list also now filters out selected comment and displays it

--- frontend/src/views/comments/comments.jsx
+++ frontend/src/views/comments/comments.jsx
@@ -1,66 +1,63 @@
 import './comments.css';
 import {CommentData} from './data.js';
-import React, {createContext, useContext, useState} from 'react';
+import React, {useState} from 'react';
 
-const CardCtx = createContext({
-    cardList: [],
-    setCardList: () => {}
-});
 
 export default function CommentsPage() {
-    const cList = CommentData.map(com => {
+    const listState = useState(CommentData);
+    const [current, setCurrent] = useState(null);
+    return (
+        <div id="comments-page">
+            <title>I'm Just Saying</title>
+            <div id="comment-stack">
+                <CommentsList lState={listState} bState={setCurrent}/>
+            </div>
+            <div id="comment-board">
+                <CommentBoard comment={current}/>
+            </div>
+        </div>
+    );
+}
+
+function CommentsList({lState, bState}) {
+    const [currentList, setCurrentList] = lState;
+    const List = currentList.map(com => {
         return(
-        <div className='comment-stack-title targetable' key={com.id} onClick={() => updateCtx(com)} >
+        <div className='comment-stack-title targetable' key={com.id} onClick={() => updatePageState(com.id, setCurrentList, bState)} >
             {com.Title}
         </div>  
         );
     });
     return (
-        <div id="comments-page">
-            <title>I'm Just Saying</title>
-            <div id="comment-stack">
-                <div id='comment-stack-list'>
-                    {cList}
-                </div>
-            </div>
-            <Corkboard />
+        <div id="comment-stack-list">
+            {List}
         </div>
     );
 }
 
-function updateCtx(com) {
-    const maxCards = 3;
-    const {cardList, setCardList} = useContext(CardCtx);
-    if (cardList.length < maxCards) {
-     setCardList(com);
-    } else {
-        console.log('Card limit reached');
-    }
+function updatePageState(id, listSetFunc, boardSetFunc) {
+    updateListState(id, listSetFunc);
+    updateBoardState(id, boardSetFunc);
 }
 
-
-function Corkboard() {
-    const [cardList, setCardList] = useState([]);
-    const value = {cardList, setCardList};
-    return (
-        <div id="corkboard-container">
-            <div id='corkboard'>
-                <CardCtx.Provider value={value}>
-                    <CommentCard props={cardList} />
-                </CardCtx.Provider>
-            </div>
-        </div>
-    );
+function updateListState(id, setFunc) {
+    const newList = CommentData.filter(com => com.id !== id);
+    setFunc(newList);
 }
 
-function CommentCard({props}) {
+function updateBoardState(id, setFunc) {
+    const newBoard = CommentData.find(com => com.id === id);
+    setFunc(newBoard);
+}
+
+function CommentBoard({comment}) {
     return (
-        <div className='comment-card'>
-            <div className='comment-card-title'>
-                {props.Title}
+        <div id="comment-board-inner">
+            <div id="comment-board-title">
+                {comment ? comment.Title : "Select a comment"}
             </div>
-            <div className='comment-card-body'>
-                {props.Body}
+            <div id="comment-board-body">
+                {comment ? comment.Body : "Select a comment to read"}
             </div>
         </div>
     );
